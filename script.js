ba21a8bf1b73dd2de8f02c1f211540e04bdbdb3d
Alefer <ribeiro.alefer@escola.pr.gov.br>
2025-09-04 10:58:19 -0300

    ok

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const avanca = document.querySelectorAll('.btn-proximo');
+const avanca = document.querySelectorA11('btn.proximo');
+const reiniciarBtn = document.getElementById('btn-reiniciar');
 
-avanca.for
+avanca.forEach(button =>
+button.addEventListener('click',function() {
+    const atual = document,querySelector('.ativo');
+    const proximoPasso = 'passo- '+this.getAttribute('data-proximo');
+
+atual.classList.remove('ativo');
+const proximoElemento= document.getElementById(proximoPasso);
+
+if(proximoElemento){
+    proximoElemento.classList.add('ativo');
+}else {
+    console.error(`Elemento com ID "${proximoPasso}" não encontrado.`)
+}
+
+ });
+});
